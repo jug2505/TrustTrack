53142301c6670ed950734e2ac77cb62b6fb0d503
Alexey Zezin <alexey.zezin@gmail.com>
2023-04-04 12:31:36 +0300

    TT-47 Добавление запроса в ФНС

--- src/decision/decision.service.ts
+++ src/decision/decision.service.ts
@@ -1,12 +1,17 @@
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { ApplicationDto, ApplicationFullDto } from '../application/dto';
-import { Decision } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { FnsResponse } from './models/fns-response.model';
+import { Decision } from '@prisma/client';
 
 @Injectable()
 export class DecisionService {
 
-    constructor(private prisma: PrismaService) {
+    constructor(private prisma: PrismaService,
+                private httpService: HttpService,
+                private confService: ConfigService) {
     }
 
     async postDecision(application: ApplicationDto): Promise<Decision> {
@@ -31,7 +36,17 @@ export class DecisionService {
             }
         })
 
-        // TODO: Поиск информации по сервисам (НБКИ и др.)
+        const decision = {
+            status: 'APPROVED',
+        }
+
+        // TODO: Поиск информации по сервисам (ФНС, НБКИ и др.)
+        const fnsResponse = await this.getFns(+dbClient.inn);
+
+        if (fnsResponse['Банкрот']['Статус']) {
+            decision.status = 'DECLINE';
+        }
+
         const dbCreditScore = await this.prisma.creditScore.create({
             data: {
                 clientId: dbClient.id,
@@ -40,9 +55,6 @@ export class DecisionService {
             }
         })
 
-        const decision = {
-            status: 'APPROVED',
-        }
 
         const dbDecision = await this.prisma.decision.create({
             data: {
@@ -63,5 +75,48 @@ export class DecisionService {
 
     postDecisionFull(applicationFull: ApplicationFullDto) {
 
+    }
+
+    async getFns(inn: number): Promise<FnsResponse> {
+        // return this.httpService.axiosRef.post(`https://api-fns.ru/api/fl_status?inn=${inn}&key=${this.confService.get('FNS_API_KEY')}`)
+
+        return new Promise<FnsResponse>((resolve) => {
+            resolve({
+                'Корректность': {
+                    'КонтрСумма': true,
+                    'Недействительный': false,
+                    'НедейстДата': 'string',
+                },
+
+                'Самозанятость': {
+                    'Статус': true,
+                    'Текст': 'string',
+                },
+
+                'ИП': {
+                    'Статус': false,
+                    'Ссылка': 'string',
+                    'Текст': 'string',
+                },
+
+                'Банкрот': {
+                    'Статус': false,
+                    'Ссылка': 'string',
+                    'Текст': 'string',
+                },
+
+                'ПоддержкаМСП': {
+                    'ФормПод': 'string',
+                    'ВидПод': 'string',
+                    'РазмПодСум': 'string',
+                    'РазмПодСр': 'string',
+                    'СрокПодМакс': 'string',
+                    'НарушПоддержкаМСП': 'string'
+                },
+
+                'ФИО': 'Фамилия Имя Отчество'
+            })
+
+        })
     }
 }
